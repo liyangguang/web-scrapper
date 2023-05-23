7265e73213e2fdb7f6b7c0adedc50f6d7cd8ad5b
Yangguang Li <liyangguangcn@gmail.com>
2023-05-23 13:22:00 -0700

    Switch to adapter-node

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,9 @@
-import adapter from '@sveltejs/adapter-auto';
-import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-node';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: [preprocess()],
+	preprocess: [vitePreprocess()],
 
 	kit: {
 		adapter: adapter()
